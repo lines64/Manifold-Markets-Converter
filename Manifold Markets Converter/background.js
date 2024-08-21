ddcf8ba653d4baeb13d84351192ce60d6c2b6b90
Mark <info@lines64.com>
2024-08-21 20:56:30 +0200

    Delete Manifold Markets Converter directory

--- Manifold Markets Converter/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ conversionEnabled: true }, function() {
-      console.log('Manifold Markets Odds Converter installed and enabled by default.');
-    });
-  });  
